e1d0aad8d95f8572864a350b9c1e318cdf67cd6d
Iashin Dmytro <iashin.d@nixsolutions.com>
2017-02-11 12:56:05 +0200

    try to handle HEROKU port

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const http = require('http');
 const app = require('./app');
 const fs = require('fs');
 const config = require('./config')(process.env.NODE_ENV);
+const PORT = process.env.PORT;
+
+console.log(process.env);
+console.log(PORT);
 
 if (process.env.NODE_ENV.includes('NIXDEV')) {
   try {
@@ -11,7 +15,7 @@ if (process.env.NODE_ENV.includes('NIXDEV')) {
 
 }
 
-http.createServer(app).listen(config.port, (err) => {
+http.createServer(app).listen(PORT || config.port, (err) => {
   if (err) {
     console.log(err);
   } else {
